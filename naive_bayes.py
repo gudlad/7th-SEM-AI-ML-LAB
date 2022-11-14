39278aeca207ef9c002f3075eeb62873966daaa9
gudladd <19a31@sdmit.in>
2022-11-14 23:24:45 +0530

    added and updated the files

--- naive_bayes.py
+++ naive_bayes.py
@@ -6,7 +6,7 @@ def loadCsv(filename):
     lines = csv.reader(open(filename, "r"))
     dataset = list(lines)
     for i in range(len(dataset)):
-        dataset[i] = [float(x) for x in dataset[i]]
+         dataset[i] = [float(x) for x in dataset[i]]
     return dataset
 
 def splitDataset(dataset, splitRatio):
